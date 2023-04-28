a5b554ac96efd6baf215a668589fff4af2361f96
Sebastiao Vitor <sebastiaovitor18@gmail.com>
2023-04-28 14:49:18 -0300

    Arrays

---
+++ ElementosDoArray.js
@@ -0,0 +1,8 @@
+// Manipulando Strings e Números
+
+// Contar elementos de um array
+console.log([
+  "a",
+  {type: "array"},
+function() { return "alo"},
+].length)
---
+++ EncontrandoPalavrasEmFrases.js
@@ -0,0 +1,6 @@
+// Manipulando Strings e Números
+
+// Verificando se o texto contém a palavra Amor
+
+let phrase = "Eu quero viver o Amor!"
+console.log(phrase.includes("Amor"))
